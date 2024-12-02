32b8081ff43200dff74d9bb69c0ff181bfaad878
eskopp <skopp.erik@gmail.com>
2024-12-02 02:12:52 +0100

    Generates a list of random moves and outputs it

--- index.py
+++ index.py
@@ -3,20 +3,37 @@ import random
 
 input_fen = "8/4N3/3k4/8/2R1n3/8/N5K1/8 b - - 0 1"
 
-def generate_random_moves(fen: str, num_moves: int = 6):
+
+def generate_random_moves(fen: str):
     board = chess.Board(fen)
-    moves = []
-    for _ in range(num_moves):
-        if board.is_game_over():
-            break
-        move = random.choice(list(board.legal_moves))
-        moves.append(move)
+    black_moves = []
+    white_moves = []
+
+    # Generiere 6 Züge (3 für Schwarz und 3 für Weiß)
+    for _ in range(6):
+        legal_moves = list(board.legal_moves)
+        move = random.choice(legal_moves)
+        move_notation = board.san(move)
+
+        if board.turn == chess.BLACK:
+            black_moves.append(move_notation)
+        else:
+            white_moves.append(move_notation)
+
         board.push(move)
-    return moves
 
-move_sequence = generate_random_moves(input_fen, num_moves=6)
+    return black_moves, white_moves, board.is_checkmate()
 
-board = chess.Board(input_fen)
-for i, move in enumerate(move_sequence, 1):
-    print(f"Move {i}: {board.san(move)}")
-    board.push(move)
+
+while True:
+    black_moves, white_moves, is_checkmate = generate_random_moves(input_fen)
+
+    # Ausgabe der Züge in der Reihenfolge Schwarz - Weiß
+    output = black_moves + white_moves
+    print(" ".join(output))
+
+    if is_checkmate:
+        print("\nSchachmatt erreicht!")
+        break
+    else:
+        print("\nKein Schachmatt. ...\n")
